48bdae7da709bd848d076e726025498170a9b2c5
gavin <gavin@kkb.com>
2019-12-23 17:11:01 +0800

    update gittest

--- com/kkb/gittest/GitDemo.java
+++ com/kkb/gittest/GitDemo.java
@@ -3,5 +3,6 @@ package com.kkb.gittest;
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello Git2!");
     }
 }
